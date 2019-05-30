b3f8a62a07b55078c5933b6498d53df35900dfaa
nakarintangmo <51126394+nakarintangmo@users.noreply.github.com>
2019-05-30 11:47:08 +0700

    [ADD]create project

---
+++ WIP-XI/src/I/Variables/Variable.java
@@ -0,0 +1,29 @@
+/*
+ * WIP XI Computer Programing 
+ */
+
+package I.Variables;
+
+
+public class Variable {
+    public static void main(String[] args) {
+        int money = 1024;
+        String name ="Tangmo";
+        boolean x=true;
+        
+        System.out.println("Hello");
+        System.out.println("WIPCAMP");
+        System.out.println("money : " + money);
+        System.out.println("วันนี้เอาเงินมา "+ money);
+        
+        int wippoWallet = 1000;
+        
+        System.out.println("wippoWallet : " + wippoWallet);
+        
+        int wippowallet = 900;
+        
+        System.out.println("wippowallet : " + wippowallet);
+        
+        
+    }
+}
